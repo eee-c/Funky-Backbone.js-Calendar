7613239b6b166d91cc001838a06643f3e149eddd
Chris Strom <eee.c@eeecooks.com>
2011-09-08 12:50:20 -0400

    Write fixture html

--- test/basic_server.test.js
+++ test/basic_server.test.js
@@ -2,12 +2,23 @@
 process.env.NODE_ENV = 'test';
 
 var app = require('../app'),
-    assert = require('assert');
+    assert = require('assert'),
+    fs = require('fs');
 
 module.exports = {
   'is a backbone.js app': function() {
     assert.response(app,
       { url: '/', timeout: 500 },
       { body: /backbone.js/ });
+  },
+
+  'generate fixture': function() {
+    assert.response(app,
+      { url: '/' },
+      function(res) {
+        fs.mkdir('fixtures', 0775, function() {
+          fs.writeFile('fixtures/homepage.html', res.body);
+        });
+      });
   }
 };
