841e89537c3269dbadab2d0efea3369c7476aed7
Chris Strom <eee.c@eeecooks.com>
2011-09-10 16:37:01 -0400

    More minor refactoring.

--- spec/javascripts/HomepageSpec.js
+++ spec/javascripts/HomepageSpec.js
@@ -1,21 +1,23 @@
 jasmine.getFixtures().preload('homepage.html');
 
-var couch_doc = '{"_id":"42",' +
-                 '"_rev":"1-2345",' +
-                 '"title":"Funk",' +
-                 '"description":"asdf",' +
-                 '"startDate":"2011-09-15"}';
-
 describe("Home", function() {
+  var server,
+      couch_doc = '{"_id":"42",' +
+                   '"_rev":"1-2345",' +
+                   '"title":"Funk",' +
+                   '"description":"asdf",' +
+                   '"startDate":"2011-09-15"}',
+      doc_list = '{"total_rows":1,"rows":[{"doc":' + couch_doc + '}]}';
+
   beforeEach(function() {
-    var server = sinon.fakeServer.create();
-    server.respondWith(
-      '{"total_rows":1,"rows":[{"doc":' + couch_doc + '}]}'
-    );
+    server = sinon.fakeServer.create();
 
+    server.respondWith(doc_list);
     loadFixtures('homepage.html');
-
     server.respond();
+  });
+
+  afterEach(function() {
     server.restore();
   });
 
