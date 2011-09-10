8844e31ea3b6cf258a2c8aada05ee6b561d14409
Chris Strom <eee.c@eeecooks.com>
2011-09-10 16:23:08 -0400

    Minor code re-org

--- spec/javascripts/HomepageSpec.js
+++ spec/javascripts/HomepageSpec.js
@@ -1,16 +1,16 @@
 jasmine.getFixtures().preload('homepage.html');
 
+var couch_doc = '{"_id":"42",' +
+                 '"_rev":"1-2345",' +
+                 '"title":"Funk",' +
+                 '"description":"asdf",' +
+                 '"startDate":"2011-09-15"}';
+
 describe("Home", function() {
   beforeEach(function() {
     var server = sinon.fakeServer.create();
     server.respondWith(
-      '/appointments',
-      [200,
-      { "Content-Type": "application/json" },
-      '{"total_rows":1,"offset":0,"rows":[' +
-        '{"id":"42","key":"42","value":{"rev":"1-2345"},' +
-        '"doc":{"_id":"42","_rev":"1-2345","title":"Funk","description":"asdf","startDate":"2011-09-15"}}' +
-        ']}']
+      '{"total_rows":1,"rows":[{"doc":' + couch_doc + '}]}'
     );
 
     loadFixtures('homepage.html');
