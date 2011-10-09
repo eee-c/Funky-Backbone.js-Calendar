6af430102e7d88a6a67b09cc8418330b2846d400
Chris Strom <eee.c@eeecooks.com>
2011-10-09 14:10:46 -0400

    Implement filtering by overriding url instead of fetch

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.get('/', function(req, res){
   });
 });
 
-app.get('/appointments', function(req, res){
+app.get('/appointments/:date', function(req, res){
   var options = {
     host: 'localhost',
     port: 5984,
-    path: '/calendar/_design/appointments/_view/by_month?key="'+ req.param('date') +'"'
+    path: '/calendar/_design/appointments/_view/by_month?key="'+ req.params.date +'"'
   };
 
   http.get(options, function(couch_response) {
@@ -51,21 +51,21 @@ app.get('/appointments', function(req, res){
 });
 
 
-app.get('/appointments/:id', function(req, res){
-  var options = {
-    host: 'localhost',
-    port: 5984,
-    path: '/calendar/' + req.params.id
-  };
+// app.get('/appointments/:id', function(req, res){
+//   var options = {
+//     host: 'localhost',
+//     port: 5984,
+//     path: '/calendar/' + req.params.id
+//   };
 
-  http.get(options, function(couch_response) {
-    console.log("Got response: %s %s:%d%s", couch_response.statusCode, options.host, options.port, options.path);
+//   http.get(options, function(couch_response) {
+//     console.log("Got response: %s %s:%d%s", couch_response.statusCode, options.host, options.port, options.path);
 
-    couch_response.pipe(res);
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
-});
+//     couch_response.pipe(res);
+//   }).on('error', function(e) {
+//     console.log("Got error: " + e.message);
+//   });
+// });
 
 app.delete('/appointments/:id', function(req, res){
   var options = {
