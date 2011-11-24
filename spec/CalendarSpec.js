3cacccef308af77c6c3862dcc7e5512e7761f907
Chris Strom <eee.c@eeecooks.com>
2011-11-24 14:33:29 -0500

    Move initial app view specs into their own context

--- spec/CalendarSpec.js
+++ spec/CalendarSpec.js
@@ -23,15 +23,6 @@ describe("Calendar", function() {
 
     $('body').append('<h1>Funky Calendar<span class="year-and-month"/></h1>');
     $('body').append('<div id="calendar"/>');
-
-    window.calendar = new Cal($('#calendar'));
-    Backbone.history.loadUrl();
-
-    // populate appointments for this month
-    server.respondWith('GET', /\/appointments/,
-      [200, { "Content-Type": "application/json" }, JSON.stringify(doc_list)]);
-    server.respond();
-
   });
 
   afterEach(function() {
@@ -59,8 +50,15 @@ describe("Calendar", function() {
 
   describe("routing", function() {
     beforeEach(function() {
-      var appointments = window.calendar.appointments;
+      window.calendar = new Cal($('#calendar'));
       Backbone.history.loadUrl();
+
+      // populate appointments for this month
+      server.respondWith('GET', /\/appointments/,
+        [200, { "Content-Type": "application/json" }, JSON.stringify(doc_list)]);
+      server.respond();
+
+      var appointments = window.calendar.appointments;
     });
 
     it("defaults to the current month", function() {
@@ -76,103 +74,116 @@ describe("Calendar", function() {
     it("sets the date of the appointment collection", function() {
       var appointments = window.calendar.appointments;
       expect(appointments.getDate())
-         .toEqual("2011-11");
+         .toEqual(year + '-' + month);
     });
   });
 
-  describe("the page title", function() {
-    it("contains the current month", function() {
-      expect($('h1')).toHaveText(/2011-11/);
-    });
-  });
+  describe("the initial view", function() {
+    beforeEach(function() {
+      window.calendar = new Cal($('#calendar'));
+      Backbone.history.loadUrl();
 
-  describe("a collection of appointments", function() {
-    it("populates the calendar with appointments", function() {
-      expect($('#' + fifteenth)).toHaveText(/Get Funky/);
-    });
-  });
-
-  describe("adding an appointment", function() {
-    it("sends clicks on day to an add dialog", function() {
-      $('#' + fourteenth).click();
-
-      var dialog = $('#calendar-add-appointment').parent();
-      expect(dialog).toBeVisible();
-      expect(dialog).toHaveText(/Add/);
-    });
-
-    it("displays the date clicked in the add dialog", function() {
-      $('#' + fourteenth).click();
-      expect($('#calendar-add-appointment')).toHaveText(new RegExp(fourteenth));
-    });
-
-    it("adds a new appointment to the UI when saved", function() {
-      $('#' + fourteenth).click();
-      $('.ok:visible').click();
-
-      var appointment = {
-        "id": "42",
-        "rev": "1-2345",
-        "startDate": fourteenth,
-        "title": "Groovy meeting",
-        "description": "asdf"
-      };
-
-      server.respondWith('POST', '/appointments', JSON.stringify(appointment));
+      // populate appointments for this month
+      server.respondWith('GET', /\/appointments/,
+        [200, { "Content-Type": "application/json" }, JSON.stringify(doc_list)]);
       server.respond();
-
-      expect($('#' + fourteenth)).toHaveText(/Groovy/);
-    });
-  });
-
-  describe("deleting an appointment", function() {
-    it("clicking the \"X\" removes records from the data store and UI", function() {
-      jasmine.Clock.useMock();
-
-      $('.delete', '#' + fifteenth).click();
-
-      server.respondWith('DELETE', '/appointments/42', '{"id":"42"}');
-      server.respond();
-
-      jasmine.Clock.tick(5000);
-      expect($('#' + fifteenth)).not.toHaveText(/Funky/);
-    });
-  });
-
-  describe("updating an appointment", function (){
-    it("sets CouchDB revision headers", function() {
-      var spy = spyOn(Backbone.Model.prototype, 'save').andCallThrough();
-      var appointment = calendar.appointments.at(0);
-
-      appointment.save({title: "Changed"});
-
-      expect(spy.mostRecentCall.args[1].headers)
-        .toEqual({ 'If-Match': '1-2345' });
     });
 
-    it("binds click events on the appointment to an edit dialog", function() {
-      $('.title', '#' + fifteenth).click();
-      expect($('#calendar-edit-appointment')).toBeVisible();
+
+    describe("the page title", function() {
+      it("contains the current month", function() {
+        expect($('h1')).toHaveText(new RegExp(year + '-' + month));
+      });
     });
 
-    it("displays model updates", function () {
-      var appointment = calendar.appointments.at(0);
-      appointment.save({title: "Changed"});
-
-      server.respondWith('PUT', '/appointments/42', '{"title":"Changed!!!"}');
-      server.respond();
-
-      expect($('#' + fifteenth)).toHaveText(/Changed/);
+    describe("a collection of appointments", function() {
+      it("populates the calendar with appointments", function() {
+        expect($('#' + fifteenth)).toHaveText(/Get Funky/);
+      });
     });
 
-    it("can edit appointments through an edit dialog", function() {
-      $('.title', '#' + fifteenth).click();
-      $('.ok:visible').click();
+    describe("adding an appointment", function() {
+      it("sends clicks on day to an add dialog", function() {
+        $('#' + fourteenth).click();
 
-      server.respondWith('PUT', '/appointments/42', '{"title":"Changed!!!"}');
-      server.respond();
+        var dialog = $('#calendar-add-appointment').parent();
+        expect(dialog).toBeVisible();
+        expect(dialog).toHaveText(/Add/);
+      });
 
-      expect($('#' + fifteenth)).toHaveText(/Changed/);
+      it("displays the date clicked in the add dialog", function() {
+        $('#' + fourteenth).click();
+        expect($('#calendar-add-appointment')).toHaveText(new RegExp(fourteenth));
+      });
+
+      it("adds a new appointment to the UI when saved", function() {
+        $('#' + fourteenth).click();
+        $('.ok:visible').click();
+
+        var appointment = {
+          "id": "42",
+          "rev": "1-2345",
+          "startDate": fourteenth,
+          "title": "Groovy meeting",
+          "description": "asdf"
+        };
+
+        server.respondWith('POST', '/appointments', JSON.stringify(appointment));
+        server.respond();
+
+        expect($('#' + fourteenth)).toHaveText(/Groovy/);
+      });
+    });
+
+    describe("deleting an appointment", function() {
+      it("clicking the \"X\" removes records from the data store and UI", function() {
+        jasmine.Clock.useMock();
+
+        $('.delete', '#' + fifteenth).click();
+
+        server.respondWith('DELETE', '/appointments/42', '{"id":"42"}');
+        server.respond();
+
+        jasmine.Clock.tick(5000);
+        expect($('#' + fifteenth)).not.toHaveText(/Funky/);
+      });
+    });
+
+    describe("updating an appointment", function (){
+      it("sets CouchDB revision headers", function() {
+        var spy = spyOn(Backbone.Model.prototype, 'save').andCallThrough();
+        var appointment = calendar.appointments.at(0);
+
+        appointment.save({title: "Changed"});
+
+        expect(spy.mostRecentCall.args[1].headers)
+          .toEqual({ 'If-Match': '1-2345' });
+      });
+
+      it("binds click events on the appointment to an edit dialog", function() {
+        $('.title', '#' + fifteenth).click();
+        expect($('#calendar-edit-appointment')).toBeVisible();
+      });
+
+      it("displays model updates", function () {
+        var appointment = calendar.appointments.at(0);
+        appointment.save({title: "Changed"});
+
+        server.respondWith('PUT', '/appointments/42', '{"title":"Changed!!!"}');
+        server.respond();
+
+        expect($('#' + fifteenth)).toHaveText(/Changed/);
+      });
+
+      it("can edit appointments through an edit dialog", function() {
+        $('.title', '#' + fifteenth).click();
+        $('.ok:visible').click();
+
+        server.respondWith('PUT', '/appointments/42', '{"title":"Changed!!!"}');
+        server.respond();
+
+        expect($('#' + fifteenth)).toHaveText(/Changed/);
+      });
     });
   });
 });
