32e0c85eea83b014f16293145c43da238fc92790
Chris Strom <eee.c@eeecooks.com>
2011-12-08 00:04:48 -0500

    Extract the appointments collection into a require.js library

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,52 +1,10 @@
 define(['jquery',
         'underscore',
         'backbone',
-        'calendar/models/appointment',
+        'calendar/collections/appointments',
         'jquery-ui'],
-  function($, _, Backbone, Appointment) {
+  function($, _, Backbone, Appointments) {
     return function(root_el) {
-
-  var Collections = (function() {
-    var Appointments = Backbone.Collection.extend({
-      model: Appointment,
-      url: '/appointments',
-      initialize: function(models, options) {
-        options || (options = {});
-        this.date = options.date;
-      },
-      fetch: function(options) {
-        options || (options = {});
-
-        var data = (options.data || {});
-        options.data = {date: this.date};
-
-        var collection = this;
-        var success = options.success;
-        options.success = function (resp, status, xhr) {
-          collection.trigger('calendar:change:date');
-          if (success) success(collection, resp);
-        };
-        options.error = function () {
-          console.log("[fetch] Dang");
-          console.log(arguments);
-        };
-
-        return Backbone.Collection.prototype.fetch.call(this, options);
-      },
-      setDate: function(date) {
-        this.date = date;
-        this.fetch();
-      },
-      getDate: function() {
-        return this.date;
-      },
-      parse: function(response) {
-        return _(response.rows).map(function(row) { return row.value ;});
-      }
-    });
-
-    return {Appointments: Appointments};
-  })();
 
   var Views = (function() {
     var Appointment = Backbone.View.extend({
@@ -529,7 +487,7 @@ define(['jquery',
 
   // Initialize the app
   var year_and_month = Helpers.to_iso8601(new Date()).substr(0,7),
-      appointments = new Collections.Appointments([], {date: year_and_month}),
+      appointments = new Appointments([], {date: year_and_month}),
       application = new Views.Application({
         collection: appointments,
         el: root_el
@@ -543,7 +501,6 @@ define(['jquery',
   }
 
   return {
-    Collections: Collections,
     Views: Views,
     Helpers: Helpers,
     appointments: appointments
---
+++ public/javascripts/calendar/collections/appointments.js
@@ -0,0 +1,40 @@
+define(['backbone', 'underscore', 'calendar/models/appointment'],
+       function(Backbone, _, Appointment) {
+  return Backbone.Collection.extend({
+    model: Appointment,
+    url: '/appointments',
+    initialize: function(models, options) {
+      options || (options = {});
+      this.date = options.date;
+    },
+    fetch: function(options) {
+      options || (options = {});
+
+      var data = (options.data || {});
+      options.data = {date: this.date};
+
+      var collection = this;
+      var success = options.success;
+      options.success = function (resp, status, xhr) {
+        collection.trigger('calendar:change:date');
+        if (success) success(collection, resp);
+      };
+      options.error = function () {
+        console.log("[fetch] Dang");
+        console.log(arguments);
+      };
+
+      return Backbone.Collection.prototype.fetch.call(this, options);
+    },
+    setDate: function(date) {
+      this.date = date;
+      this.fetch();
+    },
+    getDate: function() {
+      return this.date;
+    },
+    parse: function(response) {
+      return _(response.rows).map(function(row) { return row.value ;});
+    }
+  });
+});
