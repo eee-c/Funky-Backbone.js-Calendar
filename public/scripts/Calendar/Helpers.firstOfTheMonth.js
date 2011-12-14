04e2ef6c7433bc05e9181308d06898eb756eb207
Chris Strom <eee.c@eeecooks.com>
2011-12-14 07:49:13 -0500

    Move once used methods into the only place they're used

--- public/scripts/Calendar/Helpers.firstOfTheMonth.js
+++
@@ -1,9 +0,0 @@
-define([], function() {
-  return function(date) {
-    var parts = date.split(/\D/),
-        year = parseInt(parts[0], 10),
-        month = parseInt(parts[1], 10) - 1;
-
-    return new Date(year, month, 1);
-  };
-});
--- public/scripts/Calendar/Views.CalendarMonthBody.js
+++ public/scripts/Calendar/Views.CalendarMonthBody.js
@@ -2,9 +2,28 @@ define(function(require) {
  var Backbone = require('backbone')
    , _ = require('underscore')
    , CalendarMonthWeek = require('Calendar/Views.CalendarMonthWeek')
-   , firstOfTheMonth = require('Calendar/Helpers.firstOfTheMonth')
-   , to_iso8601 = require('Calendar/Helpers.to_iso8601')
-   , weekAfter = require('Calendar/Helpers.weekAfter');
+   , to_iso8601 = require('Calendar/Helpers.to_iso8601');
+
+  function weekAfter(date) {
+    var plus7 = new Date(date.getTime() + 7*24*60*60*1000);
+
+    // Effing daylight savings time
+    if (plus7.getHours() === 23)
+      return new Date(plus7.getTime() + 60*60*1000);
+
+    if (plus7.getHours() === 1)
+      return new Date(plus7.getTime() - 60*60*1000);
+
+    return plus7;
+  };
+
+  function firstOfTheMonth(date) {
+    var parts = date.split(/\D/),
+        year = parseInt(parts[0], 10),
+        month = parseInt(parts[1], 10) - 1;
+
+    return new Date(year, month, 1);
+  };
 
   return Backbone.View.extend({
     initialize: function(options) {
